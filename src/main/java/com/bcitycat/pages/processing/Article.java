f0a01e8249f77fbf91cf2eadd952fca892cd6f7b
Szabó Attila <neyo8826@gmail.com>
2016-03-27 16:49:51 +0200

    added processing

---
+++ src/main/java/com/bcitycat/pages/processing/Article.java
@@ -0,0 +1,10 @@
+package com.bcitycat.pages.processing;
+
+import java.util.Date;
+
+public class Article {
+    public String title;
+    public String author;
+    public String content;
+    public Date date;
+}
---
+++ src/main/java/com/bcitycat/pages/processing/Processor.java
@@ -0,0 +1,18 @@
+package com.bcitycat.pages.processing;
+
+public class Processor {
+    private static final Processor instance = new Processor();
+
+    public static Processor getInstance() {
+        return instance;
+    }
+
+    private Processor() {
+    }
+
+    /**
+     * Pushes an article for processing
+     */
+    private void pushArticle(Article article) {
+    }
+}
